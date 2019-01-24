7f8bfb7d2f297a098676f504ed00d50ca93bd59d
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-01-23 17:00:35 -0800

    Raised kickdown of m416
    Test

--- lua/cfc_m9k_stubber/stubs/m9k_assault_rifles/m9k_m416.lua
+++ lua/cfc_m9k_stubber/stubs/m9k_assault_rifles/m9k_m416.lua
@@ -3,7 +3,7 @@ if SERVER then AddCSLuaFile() end
 CFC_M9k_Stubber.registerStub(function()
     local weapon = weapons.GetStored("m9k_m416")
 
-    weapon.Primary.KickDown = 0.2
+    weapon.Primary.KickDown = 0.3
     weapon.Primary.Damage = 20
     weapon.Primary.Spread = .04
 end)
