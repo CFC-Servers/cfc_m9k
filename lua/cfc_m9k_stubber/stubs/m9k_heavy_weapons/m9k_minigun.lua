2f4db77904d810cb8928d2da4021eb4a5b0de9d2
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-02-04 19:53:37 +0100

    Update m9k_minigun.lua

--- lua/cfc_m9k_stubber/stubs/m9k_heavy_weapons/m9k_minigun.lua
+++ lua/cfc_m9k_stubber/stubs/m9k_heavy_weapons/m9k_minigun.lua
@@ -3,12 +3,12 @@ if SERVER then AddCSLuaFile() end
 CFC_M9k_Stubber.registerStub( function()
     local weapon = weapons.GetStored( "m9k_minigun" )
 
-    weapon.Primary.RPM = 1000
+    weapon.Primary.RPM = 1500
     weapon.Primary.DefaultClip = 100
     weapon.Primary.KickDown = 0.2
     weapon.Primary.KickHorizontal = 0.2
     weapon.Primary.NumShots = 2
-    weapon.Primary.Damage = 3
+    weapon.Primary.Damage = 6
     weapon.Primary.Spread = .08
     weapon.Primary.IronAccuracy = .08
 end )
