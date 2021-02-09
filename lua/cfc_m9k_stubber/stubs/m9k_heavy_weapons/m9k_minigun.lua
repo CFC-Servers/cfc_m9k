ae359c204601fcf47ff9980e40d396cc43138fc8
pierce <11771225+plally@users.noreply.github.com>
2021-02-09 13:29:55 -0500

    Update m9k_minigun.lua

--- lua/cfc_m9k_stubber/stubs/m9k_heavy_weapons/m9k_minigun.lua
+++ lua/cfc_m9k_stubber/stubs/m9k_heavy_weapons/m9k_minigun.lua
@@ -4,7 +4,7 @@ CFC_M9k_Stubber.registerStub( function()
     local weapon = weapons.GetStored( "m9k_minigun" )
 
     weapon.Primary.RPM = 1500
-    weapon.Primary.DefaultClip = 100
+    weapon.Primary.DefaultClip = 0
     weapon.Primary.KickDown = 0.2
     weapon.Primary.KickHorizontal = 0.2
     weapon.Primary.NumShots = 2
